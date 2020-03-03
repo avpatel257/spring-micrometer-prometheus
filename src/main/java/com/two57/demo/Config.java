a824f98428967a6d85925787430c7a42e4441c77
Amit Patel <amit.patel1@spglobal.com>
2020-03-03 17:02:16 -0500

    Updated metric collection

--- src/main/java/com/two57/demo/Config.java
+++ src/main/java/com/two57/demo/Config.java
@@ -1,36 +1,25 @@
 package com.two57.demo;
-//
-//import io.micrometer.core.instrument.Meter;
-//import io.micrometer.core.instrument.Tag;
-//import io.micrometer.core.instrument.config.MeterFilter;
-//import io.micrometer.core.instrument.config.MeterFilterReply;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Configuration
-//public class Config {
-//    @Bean
-//    public MeterFilter meterFilter() {
-//        return new MeterFilter() {
-//            @Override
-//            public MeterFilterReply accept(Meter.Id id) {
-//                if(id.getName().startsWith("tomcat.")) {
-//                    return MeterFilterReply.DENY;
-//                }
-//                if(id.getName().startsWith("jvm.")) {
-//                    return MeterFilterReply.DENY;
-//                }
-//                if(id.getName().startsWith("process.")) {
-//                    return MeterFilterReply.DENY;
-//                }
-//                if(id.getName().startsWith("system.")) {
-//                    return MeterFilterReply.DENY;
-//                }
-//                return MeterFilterReply.NEUTRAL;
-//            }
-//        };
-//    }
-//}
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Autowired
+    BuildProperties buildProperties;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> registerCommonTags(
+            @Value("${management.application.name:your-application-name}") String applicationName
+    ) {
+        return registry -> registry.config()
+                .commonTags(
+                        "application", applicationName,
+                        "build.version", buildProperties.getVersion());
+    }
+}
